344e6e415f88584cfdea074183c5607864579c63
Edsko de Vries <edsko@well-typed.com>
2023-07-11 09:41:08 +0200

    Use and document ghc `-haddock` flag

--- src/Generics/SOP/Haddocks/TH.hs
+++ src/Generics/SOP/Haddocks/TH.hs
@@ -9,6 +9,22 @@ import Language.Haskell.TH.Syntax
 
 import Generics.SOP.Haddocks
 
+-- | Derive 'HasHaddocks' instance for the given datatype
+--
+-- Example usage:
+--
+-- > -- | This is the first example
+-- > data Example1 =
+-- >     Constr1   -- ^ First constructor of the first example
+-- >   | Constr2   -- ^ Second constructor of the first example
+-- >   deriving stock (GHC.Generic)
+-- >   deriving anyclass (SOP.Generic, SOP.HasDatatypeInfo)
+-- >
+-- > deriveHasHaddocks ''Example1
+--
+-- NOTE: You /must/ build your code with the @ghc@ flag @-haddock@ enabled
+-- <https://downloads.haskell.org/ghc/latest/docs/users_guide/using.html#ghc-flag--haddock>,
+-- otherwise all docstrings will be 'Nothing'.
 deriveHasHaddocks :: Name -> Q [Dec]
 deriveHasHaddocks typ = fmap (:[]) $
     instanceD
